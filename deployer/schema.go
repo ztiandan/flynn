b945375eb4750cdeba86adeed4fd7a61687374da
Blaž Hrastnik <blaz.hrast@gmail.com>
2015-01-05 14:03:33 +0900

    deployer: Copy over que-go schema.
    Probably best if we somehow dynamically read the schema.sql file? That way it'd
    remain up to date.
    
    Signed-off-by: Blaž Hrastnik <blaz.hrast@gmail.com>

--- deployer/schema.go
+++ deployer/schema.go
@@ -3,16 +3,24 @@ package main
 import (
 	"github.com/flynn/flynn/Godeps/_workspace/src/github.com/flynn/go-sql"
 	"github.com/flynn/flynn/pkg/postgres"
-	"github.com/flynn/flynn/pkg/queue"
 )
 
 func migrateDB(db *sql.DB) error {
 	m := postgres.NewMigrations()
-	queueSQL, err := queue.SetupSQL("jobs")
-	if err != nil {
-		return err
-	}
-	m.Add(1, queueSQL...)
+	m.Add(1,
+		`CREATE TABLE que_jobs (
+    priority    smallint    NOT NULL DEFAULT 100,
+    run_at      timestamptz NOT NULL DEFAULT now(),
+    job_id      bigserial   NOT NULL,
+    job_class   text        NOT NULL,
+    args        json        NOT NULL DEFAULT '[]'::json,
+    error_count integer     NOT NULL DEFAULT 0,
+    last_error  text,
+    queue       text        NOT NULL DEFAULT '',
+
+    CONSTRAINT que_jobs_pkey PRIMARY KEY (queue, priority, run_at, job_id))`,
+		`COMMENT ON TABLE que_jobs IS '3'`,
+	)
 	m.Add(2,
 		`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`,
 
